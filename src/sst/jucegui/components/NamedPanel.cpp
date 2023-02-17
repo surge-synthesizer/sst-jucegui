78e896e75b1960b17c53f45d5f16dff39337e30f
Paul Walker <paul@pwjw.com>
2023-02-17 10:31:04 -0500

    Named Panel gets Tabs, Hamburgers
    And some style tweaks to go along

--- src/sst/jucegui/components/NamedPanel.cpp
+++ src/sst/jucegui/components/NamedPanel.cpp
@@ -4,8 +4,6 @@
 namespace sst::jucegui::components
 {
 
-static constexpr int outerMargin = 2, cornerRadius = 4, headerHeight = 20;
-
 NamedPanel::NamedPanel(const std::string &name)
     : style::StyleConsumer(Styles::styleClass), name(name)
 {
@@ -25,18 +23,71 @@ void NamedPanel::paint(juce::Graphics &g)
     g.fillRoundedRectangle(b.toFloat(), cornerRadius);
     g.setColour(getColour(Styles::regionBorder));
     g.drawRoundedRectangle(b.toFloat(), cornerRadius, 1);
+
+    paintHeader(g);
+}
+
+void NamedPanel::paintHeader(juce::Graphics &g)
+{
+    auto b = getLocalBounds().reduced(outerMargin);
     auto ht = b.withHeight(headerHeight).reduced(4, 0);
-    g.setFont(getFont(Styles::regionLabelFont));
-    g.setColour(getColour(Styles::regionLabelCol));
-    g.drawText(name, ht, juce::Justification::centredLeft);
+
+    auto labelWidth{0};
+    if (isTabbed)
+    {
+        labelWidth = totalTabArea.getWidth();
+
+        for (int i = 0; i < tabNames.size(); ++i)
+        {
+            auto s = tabNames[i];
+            auto p = tabPositions[i];
+
+            if (i == selectedTab)
+            {
+                g.setFont(getFont(Styles::regionLabelFont));
+                g.setColour(getColour(Styles::selectedtabcol));
+                g.drawText("[ " + s + " ]", p, juce::Justification::centred);
+            }
+            else
+            {
+                g.setFont(getFont(Styles::regionLabelFont));
+                g.setColour(getColour(Styles::regionLabelCol));
+                g.drawText(s, p, juce::Justification::centred);
+            }
+        }
+    }
+    else
+    {
+        g.setFont(getFont(Styles::regionLabelFont));
+        g.setColour(getColour(Styles::regionLabelCol));
+        g.drawText(name, ht, juce::Justification::centredLeft);
+        labelWidth = g.getCurrentFont().getStringWidth(name);
+    }
 
     g.setColour(getColour(Styles::labelrulecol));
-    auto fw = g.getCurrentFont().getStringWidth(name);
     ht = b.withHeight(headerHeight);
-    auto q =
-        ht.withTrimmedLeft(fw + 4).translated(0, ht.getHeight() / 2).withHeight(1).reduced(4, 0);
+    auto q = ht.toFloat()
+                 .withTrimmedLeft(labelWidth + 4)
+                 .translated(0, ht.getHeight() / 2 - 0.5)
+                 .withHeight(1)
+                 .reduced(4, 0)
+                 .withTrimmedRight(hasHamburger * hamburgerSize);
     g.fillRect(q);
+
+    if (hasHamburger)
+    {
+        auto hb = getHamburgerRegion();
+        auto cx = hb.getCentreX();
+        auto cy = hb.getCentreY();
+        auto w = hb.getWidth() / 3.5;
+
+        auto sz = 1.5;
+        g.fillEllipse(cx - sz, cy - sz, 2 * sz, 2 * sz);
+        g.fillEllipse(cx - w - sz, cy - sz, 2 * sz, 2 * sz);
+        g.fillEllipse(cx + w - sz, cy - sz, 2 * sz, 2 * sz);
+    }
 }
+
 juce::Rectangle<int> NamedPanel::getContentArea()
 {
     auto b = getLocalBounds().reduced(outerMargin).withTrimmedTop(headerHeight).reduced(2);
@@ -50,5 +101,62 @@ void NamedPanel::resized()
         auto c = getContentArea();
         contentAreaComp->setBounds(c);
     }
+
+    if (isTabbed)
+    {
+        resetTabState();
+    }
+}
+
+juce::Rectangle<int> NamedPanel::getHamburgerRegion()
+{
+    auto b = getLocalBounds().reduced(outerMargin);
+    auto ht = b.withHeight(headerHeight).reduced(4, 0);
+    auto hb = ht.withLeft(ht.getRight() - hamburgerSize);
+
+    return hb;
+}
+void NamedPanel::mouseDown(const juce::MouseEvent &event)
+{
+    if (hasHamburger && onHamburger && getHamburgerRegion().toFloat().contains(event.position))
+    {
+        onHamburger();
+    }
+
+    if (isTabbed)
+    {
+        auto pst = selectedTab;
+        for (int i = 0; i < tabPositions.size(); ++i)
+        {
+            if (tabPositions[i].toFloat().contains(event.position))
+            {
+                selectedTab = i;
+                if (onTabSelected)
+                    onTabSelected(i);
+            }
+        }
+        if (pst != selectedTab)
+            repaint();
+    }
+}
+
+void NamedPanel::resetTabState()
+{
+    auto f = getFont(Styles::regionLabelFont);
+
+    auto b = getLocalBounds().reduced(outerMargin);
+    auto ht = b.withHeight(headerHeight).reduced(4, 0); // the extra margin
+
+    int totalTabSize = 4;
+    tabPositions.clear();
+    for (const auto &t : tabNames)
+    {
+        auto fw = f.getStringWidth("[ " + t + " ]");
+        auto tt = ht.withLeft(totalTabSize + 4).withWidth(fw);
+        tabPositions.push_back(tt);
+        totalTabSize += fw;
+    }
+
+    totalTabArea = ht.withWidth(totalTabSize);
 }
 } // namespace sst::jucegui::components
