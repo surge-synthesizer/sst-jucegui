31d112cd938a6da77dff52762bc902ad82d2669a
Paul Walker <paul@pwjw.com>
2023-09-07 22:27:17 -0400

    More knob improvements

--- src/sst/jucegui/components/Knob.cpp
+++ src/sst/jucegui/components/Knob.cpp
@@ -94,6 +94,20 @@ void Knob::paint(juce::Graphics &g)
         return p;
     };
 
+    auto handleCenter = [this, knobarea](int r, float v) -> juce::Point<float> {
+        float dPath = 0.2;
+        float dAng = juce::MathConstants<float>::pi * (1 - dPath);
+        float pt = dAng * (2 * v - 1);
+        auto start = pt;
+
+        auto region = knobarea.reduced(r);
+
+        auto cp = region.getCentre().toFloat();
+        auto rad = std::min(region.getWidth()/2, region.getHeight()/2);
+
+        return {cp.getX() + rad * sin(pt), cp.getY() - rad * cos(pt)};
+    };
+
     auto modPath = [this, knobarea](int r, float v, float m, int direction) -> juce::Path {
         float dPath = 0.2;
         float dAng = juce::MathConstants<float>::pi * (1 - dPath);
@@ -111,7 +125,7 @@ void Knob::paint(juce::Graphics &g)
     };
 
     // outer gutter edge
-    auto pOut = pacman(0, -0.02);
+    auto pOut = pacman(0, -0.005);
     g.setColour(getColour(Styles::backgroundcol));
     g.fillPath(pOut);
 
@@ -139,14 +153,6 @@ void Knob::paint(juce::Graphics &g)
         }
     }
 
-    // Draw the handle if hovered
-    pIn = handlePath(3, source->getValue01());
-    if (isHovered)
-    {
-        g.setColour(getColour(Styles::handlecol));
-        g.fillPath(pIn);
-    }
-
     // Fill over the mess in the middle
     pIn = circle(knobarea.getWidth() / 2 - 8);
     g.setColour(juce::Colours::black);
@@ -163,21 +169,28 @@ void Knob::paint(juce::Graphics &g)
     g.fillPath(pIn);
 
     pIn = circle(knobarea.getWidth() / 2 - 10);
-    g.setGradientFill(makeGrad(0.05, 0.35));
+    g.setGradientFill(makeGrad(0.8, 0.35));
     g.fillPath(pIn);
 
     pIn = circle(knobarea.getWidth() / 2 - 11);
-    g.setGradientFill(makeGrad(0.2, 0.15));
+    g.setGradientFill(makeGrad(0.1, 0.05));
     g.fillPath(pIn);
 
-    pIn = handlePath(9, source->getValue01());
-    g.setColour(getColour(Styles::handlecol));
-    g.fillPath(pIn);
+    auto hc = handleCenter(11, source->getValue01());
 
-    pIn = circle(knobarea.getWidth() / 2 - 15);
-    g.setGradientFill(makeGrad(0.2, 0.15));
-    g.fillPath(pIn);
-
+    auto er = 2;
+    if (isHovered)
+    {
+        g.setColour(getColour(Styles::handlecol));
+        g.fillEllipse(hc.getX() - er, hc.getY() - er, er * 2 + 1, er * 2 + 1);
+    }
+    else
+    {
+        g.setColour(getColour(Styles::handlecol).withAlpha(0.4f));
+        g.fillEllipse(hc.getX() - er, hc.getY() - er, er * 2 + 1, er * 2 + 1);
+        g.setColour(getColour(Styles::handlecol).withAlpha(0.7f));
+        g.drawEllipse(hc.getX() - er, hc.getY() - er, er * 2 + 1, er * 2 + 1, 1);
+    }
     if (modulationDisplay == FROM_ACTIVE)
     {
         pOut = circle(8);
