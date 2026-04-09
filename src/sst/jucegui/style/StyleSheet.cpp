2d0b4f5791de42228a55bdba542ff4bb1274ad67
Paul <baconpaul@users.noreply.github.com>
2026-04-09 14:23:02 -0400

    Fix a warning from juce::Font() being deprecated (#217)
    making it painful to use in containers

--- src/sst/jucegui/style/StyleSheet.cpp
+++ src/sst/jucegui/style/StyleSheet.cpp
@@ -103,16 +103,7 @@ struct StyleSheetBuiltInImpl : public StyleSheet
     {
         jassert(isValidPair(c, p));
 
-#if defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable : 4996)
-#endif
-
-        fonts[c.cname][p.pname] = f;
-
-#if defined(_MSC_VER)
-#pragma warning(pop)
-#endif
+        fonts[c.cname].insert_or_assign(p.pname, f);
     }
 
     void replaceFontsWithTypeface(const juce::Typeface::Ptr &p) override
